9d9af99306a18ad8a716da6489968e9e71daea11
iWildman1 <iWildman1@hotmail.com>
2016-01-27 13:08:52 +0000

    Implemented Counter

--- js/app.js
+++ js/app.js
@@ -3,3 +3,35 @@ $(document).foundation();
 function loadVirgin() {
   window.location.href = 'http://uk.virginmoneygiving.com/fundraiser-web/fundraiser/showFundraiserPage.action;jsessionid=Zp3HBmKvofOJyhDAiDzbc5EG.lgvappprod1:server-one?userUrl=demonmedia&faId=665073&isTeam=true';
 }
+
+var deadline = new Date('2016-02-23T11:00:00');
+
+function getTimeRemaining(deadline) {
+  var t = Date.parse(deadline) - Date.parse(new Date());
+
+  var seconds = Math.floor((t/1000) % 60);
+  var minutes = Math.floor((t/1000/60) % 60);
+  var hours = Math.floor((t/(1000*60*60)) % 24);
+  var days = Math.floor( t/(1000*60*60*24) );
+
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function updateTime() {
+  var data = getTimeRemaining(deadline);
+
+  $("#counterDays").html(data.days);
+  $("#counterHours").html(data.hours);
+  $("#counterMinutes").html(data.minutes);
+  $("#counterSeconds").html(data.seconds);
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
