fc658c64e0d4e956112bc7ba748ed89497c9564b
iWildman1 <iWildman1@hotmail.com>
2016-01-27 13:03:46 +0000

    Feature Complete - Move to Alpha

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 $(document).foundation();
+
+function loadVirgin() {
+  window.location.href = 'http://uk.virginmoneygiving.com/fundraiser-web/fundraiser/showFundraiserPage.action;jsessionid=Zp3HBmKvofOJyhDAiDzbc5EG.lgvappprod1:server-one?userUrl=demonmedia&faId=665073&isTeam=true';
+}
