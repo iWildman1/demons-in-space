d33f978f208681cb9e419c357c05b4e215e3438c
iWildman1 <iWildman1@hotmail.com>
2016-02-01 15:30:07 +0000

    Added donation refresh

---
+++ js/relocate.js
@@ -0,0 +1,3 @@
+var url = "http://uk.virginmoneygiving.com/fundraiser-web/fundraiser/showFundraiserPage.action;jsessionid=Zp3HBmKvofOJyhDAiDzbc5EG.lgvappprod1:server-one?userUrl=demonmedia&faId=665073&isTeam=true";
+
+window.location = url;
